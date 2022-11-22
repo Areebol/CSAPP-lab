52c8245da7e9cf851feef802f4bd23c545d8ba02
Areebol <103118945+Areebol@users.noreply.github.com>
2022-11-22 17:06:53 +0800

    Add files via upload
    2022-11-22

---
+++ target1/ICcode3.d
@@ -0,0 +1,10 @@
+
+ICcode3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 a8 1d 56 05 	mov    $0x5561da8,%rdi
+   7:	68 fa 18 40 00       	pushq  $0x4018fa
+   c:	c3                   	retq   
---
+++ target1/ICcode3.s
@@ -0,0 +1,3 @@
+mov $0x5561da8, %rdi
+pushq $0x4018fa
+ret
